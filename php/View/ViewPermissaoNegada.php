9ef7403be83599a4f35b485e3d4951cffb833e32
Rafael Chuque <Rafa_Chuque@hotmail.com>
2017-06-04 20:33:54 -0300

    Tela de Permissao Negada Concluída

--- php/View/ViewPermissaoNegada.php
+++ php/View/ViewPermissaoNegada.php
@@ -3,8 +3,18 @@ $titulo="Login";
 include $_SESSION["root"].'includes/header.php';
 ?>
 <body>
-	<h1>Você não tem permissão para acessar essa página</h1>
-	<a class="btn btn-warning" href="javascript:history.back()">Voltar</a>
+	<div class="container" >
+		<div id="principal" >
+			<form action="postLogin" method="POST" class="center-block">
+				<div class="row">					
+					<h1  class="text-center">Você não tem permissão para acessar essa página</h1>
+					<div class="col-md-4 col-md-offset-4 text-center">
+						<a class="btn btn-warning" href="javascript:history.back()">Voltar</a>
+					</div>					
+		  		</div>
+			</form>
+		</div>
+	</div>
 <?php 
 	include $_SESSION["root"].'includes/footer.php';	
 ?>
